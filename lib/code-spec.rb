1b8939af30a2582b61383dd1ca0d9da1b89ee1f9
Kristian Mandrup <kmandrup@gmail.com>
2010-08-19 09:49:29 +0200

    removed remaining forms-spec stuff

--- lib/code-spec.rb
+++ lib/code-spec.rb
@@ -13,31 +13,8 @@ require_all File.dirname(__FILE__) + '/code_spec/matchers'
 require 'code_spec/content_helpers'
 require 'code_spec/core_ext'
 
-module FormHelperMacro  
-  def with_form_helper name
-    class_eval do
-      include get_form_matcher(name)
-    end
-  end
-  
-  protected
-
-  def get_form_matcher name
-    case name
-    when :formtastic
-      Erb::Formtastic::ContentMatchers
-    when :simpleform
-      Erb::SimpleForm::ContentMatchers
-    else
-      Erb::RailsForm::ContentMatchers
-    end    
-  end  
-end
- 
-
 RSpec.configure do |config|
-  config.include RSpec::RubyContentMatchers 
-  config.extend(FormHelperMacro) 
+  config.include RSpec::RubyContentMatchers
 end
 
                                        
