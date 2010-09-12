4f3cc23b50153fe2f70e5345d2b372d8b89e4263
Kristian Mandrup <kmandrup@gmail.com>
2010-09-12 12:49:22 +0200

    minor fix2

--- lib/code_spec/matchers/have_subclass.rb
+++ lib/code_spec/matchers/have_subclass.rb
@@ -1,22 +1,23 @@
 module RSpec::RubyContentMatchers
   class HaveSubclass < RSpec::RubyContentMatcher
-    attr_reader :klass, :superclass, :type
+    attr_reader :klass, :superclass, :type, :full_class
 
     def initialize(klass, superclass, type=nil)
-      @klass      = klass.to_s.camelize       
+      @klass      = klass.to_s.camelize              
       super @klass
       @superclass = superclass.to_s.camelize
-      @type       = type.to_s.camelize if type      
+      @type       = type.to_s.camelize if type
+      @full_class = "#{@klass}#{@type}"
     end
 
     def failure_message  
       super
-      "Expected there to be the subclass #{klass} of #{superclass}"
+      "Expected there to be the subclass #{full_class} of #{superclass}"
     end 
 
     def negative_failure_message
       super
-      "Did no expected there to be the subclass #{klass} of #{superclass}"
+      "Did no expected there to be the subclass #{full_class} of #{superclass}"
     end   
 
     protected 
